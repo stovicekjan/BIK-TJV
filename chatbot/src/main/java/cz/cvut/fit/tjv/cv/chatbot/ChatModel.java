74a31c6ba3092410e5dea880c827a705c4a28ba5
Jan Stovicek <stovija5@pc349-108.ucebny>
2018-12-01 17:12:51 +0100

    update

--- chatbot/src/main/java/cz/cvut/fit/tjv/cv/chatbot/ChatModel.java
+++ chatbot/src/main/java/cz/cvut/fit/tjv/cv/chatbot/ChatModel.java
@@ -4,12 +4,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class ChatModel {
     private final Map<String, ChatResponse> database = new HashMap(); // final: muzu priradit jenom jednou, ale muzu do toho pridavat
     private final List<String> history = new ArrayList<>();
     
-    public void submitChatResponse(String msg, ChatResponse resp) {
-        database.put(msg, resp);
+    public void submitChatResponse(String msg, String resp) {
+        if (msg == null || resp == null)
+            throw new NullPointerException("msg nor resp cannot be null");
+        
+        if (msg.isEmpty() || resp.isEmpty())
+            throw new IllegalArgumentException("msg nor resp cannot be empty");
+        
+        database.put(msg, new ChatResponse(resp));
+        
+    }
+    
+    public String chat(String msg) {
+        Objects.requireNonNull(msg); // if msg == null, throw NullPointerException
+        
+        if (database.containsKey(msg)) {
+            history.add(msg);
+            return database.get(msg).useResponse();
+        }
+        
+        else
+            throw new UnknownResponseException("message unknown");
+    }
+    
+    public boolean isAlreadyUsed(String msg) {
+        return (database.containsKey(msg) && database.get(msg).getUsage() > 1);
     }
 }
